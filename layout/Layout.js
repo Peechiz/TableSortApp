dd5a0031586dc3d4de2943c388fcfe706727bf28
Chris Impicciche <cimpicci@gmail.com>
2018-04-19 22:56:39 -0500

    adds layout

---
+++ layout/Layout.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const Layout = (props) => (
+  <div>
+    <Head>
+      <title>Table Sort</title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
+    </Head>
+    <div className="container">
+      {props.children}
+    </div>
+  </div>
+)
+
+export default Layout
